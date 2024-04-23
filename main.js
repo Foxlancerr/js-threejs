5ae0786ac840606731a12497af2be89edf5bfa13
M-Asimm <kpkprince55persia@gmail.com>
2024-04-23 15:30:10 +0500

    render cirlce

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import WebGL from "three/addons/capabilities/WebGL.js";
 import * as THREE from "three";
 
 const scene = new THREE.Scene();
@@ -12,20 +13,24 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const circle = new THREE.CircleGeometry(1, 32);
 const material = new THREE.MeshBasicMaterial({ color: "#fff" });
-const cube = new THREE.Mesh(geometry, material);
+const cube = new THREE.Mesh(circle, material);
 scene.add(cube);
-
 camera.position.z = 5;
 
 function animate() {
   requestAnimationFrame(animate);
-
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
-
   renderer.render(scene, camera);
 }
 
-animate();
+// check whether the browsers support webGl
+if (WebGL.isWebGLAvailable()) {
+  console.log("WebGL is available");
+  animate();
+} else {
+  const warning = WebGL.getWebGLErrorMessage();
+  document.getElementById("container").appendChild(warning);
+}
